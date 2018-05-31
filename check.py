9f9e61a8eba9a43621f1a0d5b4eab1df86b8015f
Connor [REDACTED] <blacksilverck35@gmail.com>
2018-05-31 02:49:41 +0000

    Initial commit

---
+++ check.py
@@ -0,0 +1,88 @@
+import discord, pycurl, sys
+
+
+bases = {
+	"https://imgur.com/{arg}" : [200],
+	"https://i.cubeupload.com/{arg}{suffix}" : [200],
+	"https://tinyimg.io/i/{arg}{suffix}" : [200]
+}
+
+suffixes = [
+	".png",
+	".jpg"
+]
+
+def try_url(url, curl_verb=False, agent=None):
+	"""Actually do the http request"""
+	try:
+		curl = pycurl.Curl()
+		curl.setopt(curl.NOBODY, True)
+		curl.setopt(curl.VERBOSE, curl_verb)
+		if(agent != None):
+			curl.setopt(curl.USERAGENT, agent)
+		curl.setopt(curl.URL, url)
+		curl.perform()
+		code = curl.getinfo(pycurl.HTTP_CODE)
+		print("> HEAD {:35}: {}".format(url, code))
+		return code
+	except:
+		return str(sys.exc_info())
+
+
+def try_urls(thing, verbose=False, agent=None):
+	urls = []
+	for key, value in bases.items():
+		if("{suffix}" in key):
+			for suffix in suffixes:
+				urls.append((key, key.format(arg=thing, suffix=suffix)))
+		else:
+			urls.append((key, key.format(arg=thing, suffix="")))
+			
+	tries = {}
+	for i, url in enumerate(urls):
+		code = try_url(url[1], agent=agent)
+		for found_code in bases[url[0]]:
+			if((code == found_code) or (verbose==True)):
+				tries[url[1]] = code
+				break
+	return tries
+
+
+
+async def try_and_send(client, M, thing, verbose=False):
+	await client.send_typing(M.channel)
+	r = try_urls(thing, verbose=verbose, agent=client.curl_agent)
+	out = ["<{}> - {}".format(url,code) for url, code in r.items()]
+	out.sort()
+	if(out == []):
+		await client.send_message(M.channel, "`{thing}`: None found.".format(thing=thing))
+	else:
+		await client.send_message(M.channel, "\n".join(out))
+
+
+
+async def check(client, M, argv):
+	ulist = argv[1:]
+	try:
+		ulist.pop(ulist.index("-v"))
+		verbose = True
+	except:
+		verbose = False
+		
+	if(ulist == []):
+		await client.send_message(M.channel, "?")
+	else:
+		for url in ulist:
+			await try_and_send(client, M, url, verbose=verbose)
+
+async def getUserAgent(client, M, argv):
+	await client.send_message(M.channel, "```\n"+client.curl_agent+"\n```")
+
+
+if(__name__ == "__main__"):
+	if("-v" in sys.argv):
+		curl_verb = True
+	else:
+		curl_verb = False
+	code = try_url("https://tinyimg.io/i/HlKgkKP.png", curl_verb=curl_verb)
+	print(code)
---
+++ main.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+import discord, json, pycurl, check
+
+
+VERSION="0.1"
+PREFIX="%"
+USERAGENT = "sbhelperbot/{bot_version} ({pycurl_version})".format(
+	bot_version=VERSION,
+	pycurl_version=pycurl.version
+)
+
+with open("token", "r") as f:
+	TOKEN = f.read().rstrip()
+
+
+client = discord.Client()
+
+client.bot_version = VERSION
+client.bot_prefix = PREFIX
+client.curl_agent = USERAGENT
+
+cmds = {
+	"check"	: check.check,
+	"useragent" : check.getUserAgent
+}
+
+@client.event
+async def on_ready():
+	print("Connected!")
+	print("----------\n")
+
+@client.event
+async def on_message(M):
+	for key, value in cmds.items():
+		if(M.content.startswith(PREFIX + key)):
+			argv = M.content.split()
+			await value(client, M, argv)
+			return
+		
+
+client.run(TOKEN)
